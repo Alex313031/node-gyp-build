48f7f0f44d90afa9b04bb28da4ff5dad60cba3ed
Alexander Frick <Alex313031@gmail.com>
2024-05-31 03:07:13 -0500

    better logging

--- bin.js
+++ bin.js
@@ -27,7 +27,7 @@ function build () {
 
   try {
     var pkg = require('node-gyp/package.json')
-    console.log('process.execPath is : ' + execpath)
+    console.log('  process.execPath is : ' + execpath)
     args = [
       execpath,
       path.join(require.resolve('node-gyp/package.json'), '..', typeof pkg.bin === 'string' ? pkg.bin : pkg.bin['node-gyp']),
